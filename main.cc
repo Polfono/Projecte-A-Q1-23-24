d0b185b0b1e54cd101c18ebb03ae3d988a09a5be
Yassin El Kaisi Rahmoun <98279345+ElKaisi@users.noreply.github.com>
2023-10-04 16:00:17 +0200

    Solo usar la parte logaritmica, la lineal ya sabemos cuanto cuesta

--- main.cc
+++ main.cc
@@ -31,20 +31,5 @@ int main() {
         Arbol.BorrarInit();
     }
     cout << "Numero medio de nodos visitados con algoritmo logaritmico: " << sum/t << endl;
-        
-    // PARTE LINEAL
-    sum = 0;
-    for(int i = 0; i < t; i++) {
-        BinaryTree Arbol(k,n);
-
-        vector<double> consulta(k);
-        for(int j = 0; j < k; j++) consulta[j] = double(rand())/RAND_MAX;
-
-        vector<double> vecino = Arbol.nearestNeighborLINEAL(consulta);
-
-        sum += Arbol.checkNumNodes();
-        Arbol.BorrarInit();
-    }
-    cout << "Numero medio de nodos visitados con algoritmo lineal: " << sum/t << endl;
 }
 

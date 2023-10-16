61babafe1a2b5b17ee834de167156636603a38a4
gorkatr <gorkaparra2003@gmail.com>
2023-10-16 18:56:06 +0200

    Main Auto

--- main.cc
+++ main.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <random>
+#include <omp.h>
 #include "Tree.hh"
 using namespace std;
 
@@ -11,8 +12,8 @@ int main() {
     default_random_engine RNG(seed);
     
 
-    int k, n, t, q, typeTree, c;
-
+    int q, t, c, typeTree;
+    /*
     cout << "Elige tipo de arbol (0-kd, 1-Relaxed, 2-Squarish): ";
     cin >> typeTree;
     while(typeTree < 0 or typeTree > 2) cin >> typeTree;
@@ -26,28 +27,47 @@ int main() {
     cin >> q;
     cout << "Ver nodos visitados por consulta? 0-no 1-sí: ";
     cin >> c;
+    */
+
 
 
     // PARTE LOGARITMICA
     
-    if(c) cout << "Nodos visitados para cada busqueda: ";
-    int sum = 0;
-    for(int i = 0; i < t; i++) {
-        BinaryTree Arbol(k, n, typeTree);
+    //if(c) cout << "Nodos visitados para cada busqueda: ";
+    cout << "Elige tipo de arbol (0-kd, 1-Relaxed, 2-Squarish): ";
+    cin >> typeTree;
 
-        for(int j = 0; j < q; j++) {
-            vector<double> consulta(k);
-            for(int l = 0; l < k; l++) consulta[l] = Uniforme(RNG);
+    q = 1;
+    t = 100;
+    c = 0;
 
-            vector<double> vecino = Arbol.nearestNeighbor(consulta);
+    cout << endl << "[";
+    for (int n = 2000; n <= 100000; n += 2000) {
+        cout << endl << "[";
+        for (int k = 2; k <= 6; ++k) {
+            int sum = 0;
 
-            int aux = Arbol.checkNumNodes();
-            if(c) cout << aux << " ";
-            sum += aux;
+            #pragma omp parallel for num_threads(omp_get_max_threads()) reduction(+:sum)
+            for(int i = 0; i < t; i++) {
+                BinaryTree Arbol(k, n, typeTree);
+
+                for(int j = 0; j < q; j++) {
+                    vector<double> consulta(k);
+                    for(int l = 0; l < k; l++) consulta[l] = Uniforme(RNG);
+
+                    vector<double> vecino = Arbol.nearestNeighbor(consulta);
+
+                    int aux = Arbol.checkNumNodes();
+                    if(c) cout << aux << " ";
+                    sum += aux;
+                }
+                Arbol.BorrarInit();
+            }
+            cout << sum/(t*q);
+            if (k != 6) cout << ",";
         }
-        Arbol.BorrarInit();
+        cout << "],";
     }
-    cout << endl << "Numero medio de nodos visitados: " << sum/(t*q) << endl;
-
+    cout << "]" << endl;
 }
 
